e4aa606e6cfe6eda6af6ff91a4cf144c77038043
채희범 <zzcjs123@gmail.com>
2026-05-10 20:56:59 +0900

    2026.05.10 기본적인 테스트 작성

--- test/issues_test.exs
+++ test/issues_test.exs
@@ -1,8 +1,8 @@
-defmodule IssuesTest do
-  use ExUnit.Case
-  doctest Issues
-
-  test "greets the world" do
-    assert Issues.hello() == :world
-  end
-end
+#defmodule IssuesTest do
+#  use ExUnit.Case
+#  doctest Issues
+#
+#  test "greets the world" do
+#    assert Issues.hello() == :world
+#  end
+#end
